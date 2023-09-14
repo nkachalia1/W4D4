48349294f0c393a716bb6f6e6ece70b9f84cfdda
JacobSeidner <jseidne1@binghamton.edu>
2023-09-14 14:02:43 -0400

    completed time complexity project

--- practice-for-ch-ruby-execution-time-differences-long-practice-main/Practice.rb
+++ practice-for-ch-ruby-execution-time-differences-long-practice-main/Practice.rb
@@ -1,10 +1,13 @@
-class Phase_1
-    list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# class TimeComp
+    list1 = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+    list2 = [5, 3, -7]
+    list3 = [2, 3, -6, 7, -6, 7]
+    list4 = [-5, -1, -3]
 
-    def my_min_list(list)
-        current_min = list[0]
-        list.length.times do
-            list.each do |el|
+    def my_min_list(arr)
+        current_min = arr[0]
+        arr.length.times do
+            arr.each do |el|
                 if el < current_min
                     current_min = el
                 end
@@ -14,14 +17,34 @@ class Phase_1
         current_min
     end
 
-    def list_2(list)
-        current_min = list[0]
-        list.each do |el|
+    def my_min_list_2(arr)
+        current_min = arr[0]
+        arr.each do |el|
             if el < current_min
                 current_min = el
             end
         end
-
         current_min
     end
-end
+
+    def subsum(arr)
+        subsums =[]
+        (0...arr.length-1).each do |start|
+            (start...arr.length).each do |stop|
+                subsums << arr[start..stop] 
+            end
+        end
+        current_max = subsums[0].sum
+        subsums.each do |subsum|
+            if subsum.sum > current_max
+                current_max = subsum.sum
+            end
+        end
+        current_max
+    end
+# end
+
+p my_min_list(list1)
+p subsum(list2)
+p subsum(list3)
+p subsum(list4)
